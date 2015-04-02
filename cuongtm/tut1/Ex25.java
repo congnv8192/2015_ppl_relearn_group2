aa1837e7663a57fd880f4809e0bec6887b1cdcc6
krazevina <cuong6147@gmail.com>
2015-04-02 23:00:09 +0700

    Grouping private folder

---
+++ cuongtm/tut1/Ex25.java
@@ -0,0 +1,35 @@
+package week2;
+
+public class Ex25 {
+  
+   /*  This program will convert a given number of eggs into
+    * the number of dozens plus the number of left over eggs.
+    * For example, 57 eggs is 4 dozen eggs plus 9 eggs left over.
+    * The number of eggs is input by the user, and the computed
+    * results are displayed.
+      */
+
+   public static void main(String[] args) {
+   
+      int eggs;    // Number of eggs, input by user.
+      int dozens;  // How many dozens in that number of eggs?
+      int extras;  // How many eggs are left over, above an integral
+                   //    number of dozens?  This value is in the
+                   //    range 0 to 11, and it is computed as
+                   //    the remainder when eggs is divided by 12.
+   
+      TextIO.put("How many eggs do you have?  ");
+      eggs = TextIO.getlnInt();
+      
+      dozens = eggs / 12;
+      extras = eggs % 12;
+      
+      TextIO.put("Your number of eggs is ");
+      TextIO.put(dozens);
+      TextIO.put(" dozen and ");
+      TextIO.put(extras);
+      TextIO.putln();
+   
+   }  // end main()
+
+}  // end class
